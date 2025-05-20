9bf55bed5e0bd85a923b2d640b025a85b30cf2fa
Shohail Parwej <102964720+shohail-DeV@users.noreply.github.com>
2025-05-20 14:26:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -30,4 +31,6 @@ def predict():
         return render_template("index.html", prediction_text=f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+    
